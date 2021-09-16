82cb55784d9ccd55ad4d4759b2fa287104e77c85
SergKrasilnikov <zergovski@mail.ru>
2021-09-16 20:39:22 +0300

    first and second task

---
+++ Python_Basic/ex02/task_2.py
@@ -0,0 +1,8 @@
+print('Задача 2. Цвета')
+
+# Исправьте программу так, чтобы в результате её выполнения
+# на экран в одну строку выводился текст: Red Blue Green RedGreenBlue Blue GreenBlue.
+r = 'Red'
+g = 'Green'
+b =  'Blue'
+print(r, b, g, r + g + b, b, g + b)
