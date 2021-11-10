28b623f5bac9494d02c5b2dc396071f2da28951b
SergKrasilnikov <zergovski@mail.ru>
2021-11-10 09:45:43 +0300

    ex14_new variant for years task

--- Python_Basic/ex14/07_years/main.py
+++ Python_Basic/ex14/07_years/main.py
@@ -1,3 +1,4 @@
+# COMMENT первый вариант
 start = int(input('Введите первый год: '))
 end = int(input('Введите второй год: '))
 
@@ -18,3 +19,27 @@ for year_loop in range(start, end + 1):
         print(year_loop)
     elif b == c and b == d:
         print(year_loop)
+
+# COMMENT второй вариант
+start = int(input('Введите первый год: '))
+end = int(input('Введите второй год: '))
+
+print('\nГода от', start, 'до', end, 'с тремя одинаковыми цифрами:')
+
+while start != end:
+    count = 0
+    for num_loop in str(start):
+        if count == 0:
+            a = num_loop
+            count += 1
+        elif count == 1:
+            b = num_loop
+            count += 1
+        elif count == 2:
+            c = num_loop
+            count += 1
+        else:
+            d = num_loop
+    if a == b and a == c or a == b and a == d or a == c and a == d or b == c and b == d:
+        print(start)
+    start += 1
