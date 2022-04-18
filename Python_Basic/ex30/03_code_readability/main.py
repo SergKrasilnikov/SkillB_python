a887b922e068bb96650466d97d3faa9c7247bb06
SergKrasilnikov <zergovski@mail.ru>
2022-04-18 10:52:36 +0300

    ex30 lambda, map(), filter(), reduce()

---
+++ Python_Basic/ex30/03_code_readability/main.py
@@ -0,0 +1,11 @@
+from typing import List
+
+#(1):
+num_list1: List[int] = [i_num for i_num in range(1001)]
+print(num_list1)
+
+#(2):
+num_list2 = []
+for i_num in range(1001):
+    num_list2.append(i_num)
+print(num_list2)
