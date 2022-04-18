d46c7c92da62922b63c4d43e253eb27aa76bdd61
SergKrasilnikov <zergovski@mail.ru>
2022-04-18 13:27:07 +0300

    ex30

--- Python_Basic/ex30/03_code_readability/main.py
+++ Python_Basic/ex30/03_code_readability/main.py
@@ -1,11 +1,13 @@
 from typing import List
 
 #(1):
-num_list1: List[int] = [i_num for i_num in range(1001)]
+num_list1: List[int] = [i_num for i_num in range(2, 1001) if i_num % 2 != 0 and i_num % 3 != 0 or i_num == 2 or i_num == 3]
 print(num_list1)
 
 #(2):
 num_list2 = []
-for i_num in range(1001):
-    num_list2.append(i_num)
+for i_num in range(2, 1001):
+    if i_num % 2 != 0 and i_num % 3 != 0 or i_num == 2 or i_num == 3:
+        num_list2.append(i_num)
+
 print(num_list2)
